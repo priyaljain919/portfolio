dd1f41e87c2de49113a17e698ac850d978f9df7e
Priyal Jain <priyaljain919@gmail.com>
2020-06-26 22:52:56 +0530

    code updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDpq8CP_Kiift3W0e7BRpBe6J948USZo3w",
+    projectId: "portfolio-51046",
+  }
 };
