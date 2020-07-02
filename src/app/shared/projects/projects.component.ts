8e123cb72f28ada8780c3f2f021ec56bc2577d4b
Priyal Jain <priyaljain919@gmail.com>
2020-07-02 15:49:25 +0530

    code updated

--- src/app/shared/projects/projects.component.ts
+++ src/app/shared/projects/projects.component.ts
@@ -14,29 +14,30 @@ export class ProjectsComponent implements OnInit {
 
     this.projectList = [
       {
-        name: 'Delivr',
-        technology: 'HTML,SCSS,Angular',
-        image: 'assets/images/delivr.PNG',
-        link: 'https://delivr.biz/'
+        name: 'IWD India',
+        image: 'assets/images/iwd-india.PNG',
+        link: 'https://wtmindia-iwd20.web.app/',
+        desc: 'It\'s basically for Google Women Techmakers global event website for registration, agenda and session for the event. Implemented whole project using Vue, HTML, CSS'
       },
       {
         name: 'Tyko',
-        technology: 'HTML,SCSS,Angular',
         image: 'assets/images/tyko.PNG',
-        link: 'https://tyko.in/'
+        link: 'https://tyko.in/',
+        desc: 'Tyko is type of portal website where user able to choose to become professional or expert domain and show the details about user including like, comment, share and list of timeline post. Implemented whole project using Angular, NgRx Store, HTML, SCSS'
       },
       {
-        name: 'Framebazaar',
-        technology: 'HTML,SCSS,Angular',
-        image: 'assets/images/framebazaar.PNG',
-        link: 'https://framebazaar.com/'
+        name: 'Delivr',
+        image: 'assets/images/delivr.PNG',
+        link: 'https://delivr.biz/',
+        desc: 'It\'s an app that connects you to the nearest delivery partner who can make purchases, pick up items from any store or restaurant in the city and bring them to you. Implemented whole project using Angular, HTML, SCSS'
       },
       {
-        name: 'IWD India',
-        technology: 'HTML,CSS,Vue',
-        image: 'assets/images/iwd-india.PNG',
-        link: 'https://wtmindia-iwd20.web.app/'
-      }
+        name: 'Showground',
+        image: 'assets/images/showground.PNG',
+        link: 'https://showground-app.com/',
+        desc: 'Showground is an app to help you find out where all the best local music is with the best possible user experience. Implemented web page using HTML, CSS, JavaScript and JQuery.'
+      },
+      
     ]
   }
 
